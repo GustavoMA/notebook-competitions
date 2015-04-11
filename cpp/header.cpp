93ae68e65912899bb0574f43e7a34fbf276b694d
Gustavo Alves <gugamalves@yahoo.com.br>
2015-04-10 23:08:22 -0300

    add header cpp

---
+++ cpp/header.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long      ll;
+typedef pair<int, int> ii;
+typedef vector<int>    vi;
+typedef vector<ll>    vll;
+typedef vector<ii>    vii;
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sc second
+#define INF 2000000000
+#define MAX_N 100100
